d9822ad2ef68279f3256f689503f26702a2a1a65
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-03 11:16:01 -0600

    feat: create custom hook for storing state

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export default function useLocalStorage(
+  key: string,
+  initialValue: string | Function
+) {
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(key);
+    if (jsonValue != null) {
+      return JSON.parse(jsonValue);
+    }
+
+    if (initialValue instanceof Function) {
+      return initialValue();
+    }
+
+    return initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
