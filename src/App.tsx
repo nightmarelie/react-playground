bd2be27d061deec4960160bc4d85485641cb4ccf
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-05 22:24:41 -0600

    feat: add use case of using debounce hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, ChangeEvent, useTransition } from "react";
 import ListComponent, { largeList } from "./components/ListComponent";
 import Modal from "./components/Modal";
 import useLocalSotrage from "./hooks/useLocalStorage";
+import useDebounceEffect from "./hooks/useDebounceEffect";
 import { ThemeContext } from "./contexts/ThemeContext";
 
 function App() {
@@ -10,6 +11,19 @@ function App() {
   const [list, setList] = useState(largeList);
   const [isPanding, startTransition] = useTransition();
   const [theme, setTheme] = useState("dark");
+
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  useDebounceEffect(
+    () => {
+      fetch(`https://example.com?q=${query}`)
+        .then((res) => res.json())
+        .then((data) => setData(data));
+    },
+    [query],
+    500
+  );
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setName(e.target.value);
