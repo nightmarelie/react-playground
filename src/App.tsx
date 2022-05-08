041f459afb78b3f41ed29db4642db2ff92e0c548
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-07 23:18:22 -0600

    feat: clear modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useState, ChangeEvent, useTransition } from "react";
 import ListComponent, { largeList } from "./components/ListComponent";
-import Modal from "./components/Modal";
 import useLocalSotrage from "./hooks/useLocalStorage";
 import useDebounceEffect from "./hooks/useDebounceEffect";
 import { ThemeContext } from "./contexts/ThemeContext";
 
 function App() {
-  const [open, setOpen] = useState(false);
   const [name, setName] = useLocalSotrage("name", "");
   const [list, setList] = useState(largeList);
   const [isPanding, startTransition] = useTransition();
@@ -35,10 +33,6 @@ function App() {
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className="component" onClick={() => console.log(111121212)}>
-        <button onClick={() => setOpen(true)}>Open Modal</button>
-        <Modal isOpen={open} onClose={() => setOpen(false)}>
-          Fancy Modal
-        </Modal>
         <input type="text" value={name} onChange={handleChange} />
         {isPanding ? (
           <div>Loading...</div>
